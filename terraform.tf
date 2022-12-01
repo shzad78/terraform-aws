827ed53f15af36515cf65102cce8181f59a683d0
shzad78 <shzad78@gmail.com>
2022-12-01 14:42:05 +0100

    s3-bucket

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,12 @@
 provider "aws" {
+    region = "us-east-1"
    profile = "babar-terraform"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "babar-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
